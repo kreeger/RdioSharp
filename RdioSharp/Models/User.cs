b5f3ee54089b6ec3435d8f1ef3e7b47b8c87b391
Benjamin Kreeger <benjaminkreeger@gmail.com>
2011-03-23 13:40:47 -0500

    Still deciding on which OAuth library to use.

---
+++ RdioSharp/Models/User.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace RdioSharp.Models
+{
+    public class User : RdioObject
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string LibraryVersion { get; set; }
+        public string Gender { get; set; }
+        public string Username { get; set; }
+        public string LastSongPlayed { get; set; }
+        public string DisplayName { get; set; }
+        public int TrackCount { get; set; }
+        public DateTime LastSongPlayTime { get; set; }
+
+        internal User(string key, string url, string icon, string baseIcon, RdioType rdioType,
+                      string firstName, string lastName, string libraryVersion, string gender,
+                      string username = null, string lastSongPlayed = null, string displayName = null,
+                      int trackCount = 0, DateTime lastSongPlayTime = new DateTime())
+            : base(key, url, icon, baseIcon, rdioType)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            LibraryVersion = libraryVersion;
+            Gender = gender;
+            Username = username;
+            LastSongPlayed = lastSongPlayed;
+            DisplayName = displayName;
+            TrackCount = trackCount;
+            LastSongPlayTime = lastSongPlayTime;
+        }
+    }
+}
--- RdioSharp/RdioManager.cs
+++ RdioSharp/RdioManager.cs
@@ -1,4 +1,6 @@
-﻿namespace RdioSharp
+﻿using RdioSharp.Models;
+
+namespace RdioSharp
 {
     public interface IRdioManager
     {
@@ -6,9 +8,51 @@
     
     internal class RdioManager : IRdioManager
     {
-        internal RdioManager()
-        {
+        private string _consumerKey;
+        private string _consumerSecret;
+        private string _accessKey;
+        private string _accessSecret;
 
+        private const string API_URI = "http://api.rdio.com/1/";
+
+        internal RdioManager(string consumerKey = null, string consumerSecret = null,
+                             string accessKey = null, string accessSecret = null)
+        {
+            SetCredentials(consumerKey, consumerSecret, accessKey, accessSecret);
+        }
+
+        /// <summary>
+        /// Sets credentials for the Rdio API manager.
+        /// </summary>
+        /// <param name="consumerKey">The OAuth consumer key.</param>
+        /// <param name="consumerSecret">The OAuth consumer secret.</param>
+        /// <param name="accessKey">The OAuth access token key.</param>
+        /// <param name="accessSecret">The OAuth access token key.</param>
+        private void SetCredentials(string consumerKey = null, string consumerSecret = null,
+                                string accessKey = null, string accessSecret = null)
+        {
+            if (!string.IsNullOrEmpty(consumerKey) && !string.IsNullOrEmpty(consumerSecret))
+            {
+                _consumerKey = consumerKey;
+                _consumerSecret = consumerSecret;
+            }
+
+            if (!string.IsNullOrEmpty(accessKey) && !string.IsNullOrEmpty(accessSecret))
+            {
+                _accessKey = accessKey;
+                _accessSecret = accessSecret;
+            }
+        }
+
+        /// <summary>
+        /// Find a user either by email address or by their username.
+        /// Exactly one of email or vanityName must be supplied.
+        /// </summary>
+        /// <param name="email">An email address.</param>
+        /// <param name="vanityName">A username.</param>
+        /// <returns></returns>
+        public User FindUser(string email = null, string vanityName = null)
+        {
         }
     }
 }
