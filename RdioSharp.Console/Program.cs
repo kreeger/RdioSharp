92291c487a4279255be15566ad94b130bb6e1572
Benjamin Kreeger <benjaminkreeger@gmail.com>
2011-03-24 16:07:14 -0500

    Updates to the console app.

--- RdioSharp.Console/Program.cs
+++ RdioSharp.Console/Program.cs
@@ -1,37 +1,38 @@
 ﻿using System.Diagnostics;
+
 namespace RdioSharp.Console
 {
-    class Program
+    public static class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            var consumerKey = Constants.ConsumerKey;
-            var consumerSecret = Constants.ConsumerSecret;
-            var accessKey = Constants.AccessKey;
-            var accessSecret = Constants.AccessSecret;
-            var _manager = new RdioManager(consumerKey, consumerSecret, accessKey, accessSecret);
+            const string consumerKey = Constants.ConsumerKey;
+            const string consumerSecret = Constants.ConsumerSecret;
+            const string accessKey = Constants.AccessKey;
+            const string accessSecret = Constants.AccessSecret;
+            var manager = new RdioManager(consumerKey, consumerSecret, accessKey, accessSecret);
 
             System.Console.WriteLine("Finding user.");
-            var user = _manager.FindUser(email: "benjaminkreeger@gmail.com");
+            var user = manager.FindUser(email: "benjaminkreeger@gmail.com");
             System.Console.WriteLine(user);
 
-            if (_manager.IsAuthorized)
+            if (manager.IsAuthorized)
             {
                 System.Console.WriteLine("Getting request token.");
-                _manager.GenerateRequestTokenAndLoginUrl();
-                System.Console.WriteLine(string.Format("Authorize at this url: {0}", _manager.LoginUrl));
-                var p = new Process { StartInfo = new ProcessStartInfo(_manager.LoginUrl) };
+                manager.GenerateRequestTokenAndLoginUrl();
+                System.Console.WriteLine(string.Format("Authorize at this url: {0}", manager.LoginUrl));
+                var p = new Process { StartInfo = new ProcessStartInfo(manager.LoginUrl) };
                 p.Start();
                 System.Console.Write("What is the pin number? ");
                 var pin = System.Console.ReadLine();
 
                 System.Console.WriteLine(string.Format("Authorizing for pin # {0}.", pin));
-                _manager.Authorize(pin);
-                System.Console.WriteLine("Now have credentials!\nAccess key: {0}\nAccess key secret: {1}", _manager.AccessKey, _manager.AccessKeySecret);
+                manager.Authorize(pin);
+                System.Console.WriteLine("Now have credentials!\nAccess key: {0}\nAccess key secret: {1}", manager.AccessKey, manager.AccessKeySecret);
             }
 
             System.Console.WriteLine("Getting current user.");
-            var currentUser = _manager.CurrentUser();
+            var currentUser = manager.CurrentUser();
             System.Console.WriteLine(currentUser);
             System.Console.ReadKey();
         }
