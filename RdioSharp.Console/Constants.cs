9449a0cf1995f7a5688a14b9f60a9e2041a1b2e9
Benjamin Kreeger <benjaminkreeger@gmail.com>
2011-05-07 13:32:57 -0500

    Added empty constants file.

---
+++ RdioSharp.Console/Constants.cs
@@ -0,0 +1,10 @@
+﻿namespace RdioSharp.Console
+{
+    public class Constants
+    {
+        public const string ConsumerKey = "";
+        public const string ConsumerSecret = "";
+        public const string AccessKey = "";
+        public const string AccessSecret = "";
+    }
+}
